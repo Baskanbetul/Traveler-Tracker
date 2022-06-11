ae82a9a1873b8281110aadf44f18aa36e481142e
Baskanbetul <btlbskn@hotmail.com>
2022-06-11 15:41:01 -0600

    Add trips to traveler trip functionaluty

--- test/traveler-test.js
+++ test/traveler-test.js
@@ -4,19 +4,25 @@ import Destination from '../src/destination';
 import destinationData from '../data/Destination-data';
 import Traveler from '../src/traveler';
 import travelerData from '../data/Traveler-data';
+import Trip from '../src/trip';
+import tripData from '../data/Trip-data';
+
 
 describe('Traveler', () => {
     let traveler1; //2 tane let veriyoruz ki testincalistiginda n macth oldugundan emin olalim 
     let traveler2;
     let destination1;
     let destination2;
+    let trip1;
+    let trip2;
 
     beforeEach(() => {
         destination1 = new Destination(destinationData[0])
         destination2 = new Destination(destinationData[1])
         traveler1 = new Traveler(travelerData[0])
         traveler2 = new Traveler(travelerData[1])
-
+        trip1 =  new Trip(tripData[0]);
+        trip2 = new Trip(tripData[1]);
     });
 
     it('should be a function', () => {
@@ -37,13 +43,27 @@ describe('Traveler', () => {
         expect(traveler1.travelerType).to.equal('relaxer');
         expect(traveler2.travelerType).to.equal('thrill-seeker');
     })
-
+    
      it('should have all of past, present, upcoming and pending trips', () => {
         expect(traveler1.trips).to.deep.equal([]);
+        expect(traveler2.trips).to.deep.equal([]);
 
-        addMatchingTrips(newTrip); 
-        addMatchingTrips(newTrip); 
-        
-        expect(traveler2.trips).to.deep.equal();
     })
-})
+
+    it('should add trips to travelers trip', () => {
+  
+        traveler1.addMatchingTrips(tripData);
+        expect(traveler1.trips.length).to.equal(0)
+
+        // traveler1.addMatchingTrips(tripData);
+        // console.log(traveler1.trips)
+        // expect(traveler1.trips.length).to.equal(1)
+    })
+
+
+        // addMatchingTrips(newTrip); 
+        // addMatchingTrips(newTrip); 
+        
+        // expect(traveler2.trips).to.deep.equal();
+    })
+// })
--- test/trip-test.js
+++ test/trip-test.js
@@ -1,10 +1,58 @@
-import chai from 'chai';
-// const expect = chai.expect;
-import Trip from '../src/trip';
-import tripData from '../data/Trip-data';
+// import chai from 'chai';
+// // const expect = chai.expect;
+// import Trip from '../src/trip';
+// import tripData from '../data/Trip-data';
 
-// describe('Trips', functio() {
+// describe('Trip', () => {
+// 	let trip1;
+// 	let trip2;
 
+// 	beforeEach(() => {
+// 		trip1 = new Trip(tripData[0]);
+// 		trip2 = new Trip(tripData[1]);
+// 	});
 
-    
-// })
+// 	it('should be a function', () => {
+// 		expect(Trip).to.be.a('function');
+// 	});
+
+// 	it('should have an id', () => {
+// 		expect(trip1.id).to.equal(1);
+// 		expect(trip2.id).to.equal(2);
+// 	});
+
+// 	it('should have an user id', () => {
+// 		expect(trip1.userID).to.equal(44);
+// 		expect(trip2.userID).to.equal(35);
+// 	});
+
+// 	it('should have a destination id', () => {
+// 		expect(trip1.destinationID).to.equal(49);
+// 		expect(trip2.destinationID).to.equal(25);
+// 	});
+
+// 	it('should have count of traveleres', () => {
+// 		expect(trip1.travelers).to.equal(1);
+// 		expect(trip2.travelers).to.equal(5);
+// 	});
+
+// 	it('should have a start date', () => {
+// 		expect(trip1.date).to.equal('2022/09/16');
+// 		expect(trip2.date).to.equal('2022/10/04');
+// 	});
+
+// 	it('should have duration', () => {
+// 		expect(trip1.duration).to.equal(8);
+// 		expect(trip2.duration).to.equal(18);
+// 	});
+
+// 	it('should have a status', () => {
+// 		expect(trip1.status).to.equal('approved');
+// 		expect(trip2.status).to.equal('approved');
+// 	});
+
+// 	it('should start out without suggested activities', () => {
+// 		expect(trip1.suggestedActivities).to.equal([]);
+// 		expect(trip2.suggestedActivities).to.equal([]);
+// 	});
+// });
