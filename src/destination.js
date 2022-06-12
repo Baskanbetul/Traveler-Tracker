405f328324369953cf077732801d6f09dafe036a
Baskanbetul <btlbskn@hotmail.com>
2022-06-12 15:22:26 -0600

    Add spaces

--- src/destination.js
+++ src/destination.js
@@ -1,12 +1,41 @@
 class Destination {
-    constructor(destinationInfo) {
-        this.id = destinationInfo.id,
-        this.name = destinationInfo.destination,
-        this.lodging = destinationInfo.estimatedLodgingCostPerDay,
-        this.flights = destinationInfo.estimatedFlightCostPerPerson,
-        this.image = destinationInfo.image,
-        this.alt = destinationInfo.alt  || "city with boats on the water during the day time"
-    }
+	constructor(destinationData) {
+		this.data = destinationData;
+	}
+
+	calculateTripsExpenses(duration, travelers, id) {
+
+		let agentFee = 0;
+		const userTrip = this.data.filter((destination) => destination.id === id);
+		const expenseForTrip = userTrip.reduce((acc, cur) => {
+			acc += cur.estimatedLodgingCostPerDay * duration;
+			acc += cur.estimatedFlightCostPerPerson * travelers;
+			return acc;
+		}, 0);
+		agentFee = expenseForTrip * 0.1;
+		return expenseForTrip + agentFee;
+	}
+
+	getCurrentYear() {
+		var today = new Date();
+		var yyyy = today.getFullYear();
+		return yyyy;
+	}
+
+	calculateTotalTravelExpenses(tripData) {
+		let agentFee = 1.1;
+		const currentYear = this.getCurrentYear();
+		const expenseForYearlyTrip = tripData.reduce((totalPrice, trip) => {
+			let getdestinations = this.data.forEach((destination) => {
+				if (destination.id === trip.destinationID && trip.date.includes(currentYear)) {
+					totalPrice += destination.estimatedLodgingCostPerDay * trip.duration;
+					totalPrice += destination.estimatedFlightCostPerPerson * trip.travelers;
+				}
+			});
+			return Number((totalPrice * agentFee).toFixed(2));
+		}, 0);
+		return expenseForYearlyTrip;
+	}
 }
 
 export default Destination;
